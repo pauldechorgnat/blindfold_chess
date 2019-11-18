15b37f7477ab39cf2dace810ca112a150da7b885
Paul Dechorgnat <pdechorgnat@yahoo.fr>
2019-11-18 20:40:53 +0100

    adding the two steps knight exercise

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash
 from flask_socketio import SocketIO
-from utils.assets import color_cases, load_bishop_moves, load_knight_moves
+from utils.assets import color_cases, load_bishop_moves, load_knight_moves, load_knight_two_steps_moves
 import random
 
 app = Flask(__name__)
@@ -8,6 +8,7 @@ socketio = SocketIO(app=app)
 
 bishop_moves = load_bishop_moves()
 knight_moves = load_knight_moves()
+knight_two_steps_moves = load_knight_two_steps_moves()
 
 
 @app.route('/')
@@ -29,6 +30,11 @@ def bishop_moves_exercise():
 @app.route('/knight_moves')
 def knight_moves_exercise():
     return render_template('knight_moves.html')
+
+
+@app.route('/knight_moves2')
+def knight_two_steps_moves_exercise():
+    return render_template('knight_moves_two_steps.html')
 
 
 @socketio.on('check_color')
@@ -105,6 +111,38 @@ def check_knight_move(data):
         new_stop = random.choice(list(color_cases.keys()))
 
     socketio.emit('knight_move_checked',
+                  {
+                      'new_start': new_start,
+                      'new_stop': new_stop,
+                      'response': response,
+                      'score': score,
+                      'old_start': old_start,
+                      'old_stop': old_stop
+                  }
+                  )
+
+
+@socketio.on('check_knight_two_steps_move')
+def check_knight_two_steps_move(data):
+    old_stop = data['old_stop']
+    old_start = data['old_start']
+    decision = data['decision']
+
+    available_moves = knight_two_steps_moves[old_start]
+    if ((old_stop in available_moves) and (decision == 1)) or ((old_stop not in available_moves) and (decision == 0)):
+        response = 'Good!'
+        score = 1
+    else:
+        response = 'Wrong!'
+        score = -1
+    flash(response)
+    new_start = random.choice(list(color_cases.keys()))
+    if random.uniform(0, 1) > .6:
+        new_stop = random.choice(list(knight_two_steps_moves[new_start]))
+    else:
+        new_stop = random.choice(list(color_cases.keys()))
+
+    socketio.emit('knight_two_steps_move_checked',
                   {
                       'new_start': new_start,
                       'new_stop': new_stop,
