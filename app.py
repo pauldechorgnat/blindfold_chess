7a307df11118770f8a3cf29396baec6832df90c5
Paul Dechorgnat <pdechorgnat@yahoo.fr>
2019-11-18 17:34:03 +0100

    adding bishop quizz

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, flash
 from flask_socketio import SocketIO
-from utils.assets import color_cases
+from utils.assets import color_cases, load_bishop_moves
 import random
 
 app = Flask(__name__)
 socketio = SocketIO(app=app)
+
+bishop_moves = load_bishop_moves()
+print(type(bishop_moves))
 
 
 @app.route('/')
@@ -16,6 +19,11 @@ def index():
 @app.route('/colors')
 def colors():
     return render_template('colors.html')
+
+
+@app.route('/bishop_moves')
+def bishop_moves():
+    return render_template('bishop_moves.html')
 
 
 @socketio.on('check_color')
@@ -39,5 +47,37 @@ def check_color(data):
                   )
 
 
+@socketio.on('check_bishop_move')
+def check_bishop_move(data):
+    print(data)
+    old_stop = data['old_stop']
+    old_start = data['old_start']
+    decision = data['decision']
+    bishop_moves = load_bishop_moves()
+    print(type(bishop_moves))
+    available_moves = bishop_moves[old_start]
+    if ((old_stop in available_moves) and (decision == 1)) or ((old_stop not in available_moves) and (decision == 0)):
+        response = 'Good!'
+        score = 1
+    else:
+        response = 'Wrong!'
+        score = 0
+    flash(response)
+    new_start = random.choice(list(color_cases.keys()))
+    new_stop = random.choice(list(color_cases.keys()))
+
+
+    socketio.emit('bishop_move_checked',
+                  {
+                      'new_start': new_start,
+                      'new_stop': new_stop,
+                      'response': response,
+                      'score': score,
+                      'old_start': old_start,
+                      'old_stop': old_stop
+                  }
+                  )
+
+
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True)
