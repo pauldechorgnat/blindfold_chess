3aba70042ef9641dc94e274389cf670b3146de71
Paul Dechorgnat <pdechorgnat@yahoo.fr>
2019-12-29 12:09:46 +0100

    Getting rid off sockets for quasi all pages

--- app.py
+++ app.py
@@ -35,13 +35,15 @@ def bishop_moves_exercise():
 @app.route('/knight_moves')
 def knight_moves_exercise():
     return render_template('knight_moves.html',
-                           title='Knight Moves I exercise')
+                           title='Knight Moves I exercise',
+                           moves=knight_moves)
 
 
 @app.route('/knight_moves2')
 def knight_two_steps_moves_exercise():
     return render_template('knight_moves_two_steps.html',
-                           title='Knight Moves II exercise')
+                           title='Knight Moves II exercise',
+                           moves=knight_two_steps_moves)
 
 
 @app.route('/test')
@@ -49,68 +51,6 @@ def test():
     return render_template('visualization_test.html')
 
 
-@socketio.on('check_knight_move')
-def check_knight_move(data):
-    old_stop = data['old_stop']
-    old_start = data['old_start']
-    decision = data['decision']
-
-    available_moves = knight_moves[old_start]
-    if ((old_stop in available_moves) and (decision == 1)) or ((old_stop not in available_moves) and (decision == 0)):
-        response = 'Good!'
-        score = 1
-    else:
-        response = 'Wrong!'
-        score = -1
-    flash(response)
-    new_start = random.choice(list(color_cases.keys()))
-    if random.uniform(0, 1) > .6:
-        new_stop = random.choice(list(knight_moves[new_start]))
-    else:
-        new_stop = random.choice(list(color_cases.keys()))
-
-    socketio.emit('knight_move_checked',
-                  {
-                      'new_start': new_start,
-                      'new_stop': new_stop,
-                      'response': response,
-                      'score': score,
-                      'old_start': old_start,
-                      'old_stop': old_stop
-                  }
-                  )
-
-
-@socketio.on('check_knight_two_steps_move')
-def check_knight_two_steps_move(data):
-    old_stop = data['old_stop']
-    old_start = data['old_start']
-    decision = data['decision']
-
-    available_moves = knight_two_steps_moves[old_start]
-    if ((old_stop in available_moves) and (decision == 1)) or ((old_stop not in available_moves) and (decision == 0)):
-        response = 'Good!'
-        score = 1
-    else:
-        response = 'Wrong!'
-        score = -1
-    flash(response)
-    new_start = random.choice(list(color_cases.keys()))
-    if random.uniform(0, 1) > .6:
-        new_stop = random.choice(list(knight_two_steps_moves[new_start]))
-    else:
-        new_stop = random.choice(list(color_cases.keys()))
-
-    socketio.emit('knight_two_steps_move_checked',
-                  {
-                      'new_start': new_start,
-                      'new_stop': new_stop,
-                      'response': response,
-                      'score': score,
-                      'old_start': old_start,
-                      'old_stop': old_stop
-                  }
-                  )
 
 
 @socketio.on('new_game_visualization')
